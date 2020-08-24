2bea06655e6056e1721bc7993875b79f25603482
David Barbour <danceguy303@gmail.com>
2020-08-24 02:33:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
- if int % 3 == 0
+ if int == % 3
   "Fizz"
  elsif int % 5 == 0
    "Buzz"
- elsif int % 15 == 3 && 5
+ elsif int % 15 == 0
    "FizzBuzz"
  else int % 4 == 0
    nil
